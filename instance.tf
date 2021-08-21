6f41d7091a5d2c702c844e0990588032580b6fd2
Daniel <padillaortiz.daniel099@gmail.com>
2021-08-21 02:24:06 +0000

    added security groups

--- instance.tf
+++ instance.tf
@@ -10,3 +10,25 @@ resource "aws_instance" "The-Empire-Basic" {
 		command = "echo 'this is a test'"
 	}
 }
+
+resource "aws_security_group" "allow_traffic" {
+	name = "death_ssh_traffic"
+	description = "allow traffic from ssh"
+	ingress {
+		description = "SSH"
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["69.216.99.86/32"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "Allow SSH"
+	}
+	
+}
