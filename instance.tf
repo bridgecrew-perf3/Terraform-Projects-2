6640157eec21536ff76176dbb5518acaab09bf8e
Daniel <padillaortiz.daniel099@gmail.com>
2021-08-21 02:52:59 +0000

    needed the 'vpc_security_groups_ids' variable and corresponding value to SSH to my AWS instance

--- instance.tf
+++ instance.tf
@@ -2,11 +2,36 @@ resource "aws_instance" "The-Empire-Basic" {
 	ami = var.AMIS[var.AWS_REGION]
 	instance_type = "t2.micro"
 	key_name = "dp-scarif"
+	vpc_security_group_ids = [aws_security_group.allow_traffic.id]
 	tags = {
 		Name = "The Empire - DP Terraform Test"
 	} 
 	
+	
+	#sends the echo command to the aws linux instance	
 	provisioner "local-exec" { 
 		command = "echo 'this is a test'"
 	}
+}
+
+resource "aws_security_group" "allow_traffic" {
+	name = "death_ssh_traffic"
+	description = "allow traffic from ssh"
+	ingress {
+		description = "SSH"
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["69.216.99.86/32"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	tags = {
+		Name = "Allow SSH"
+	}
+	
 }
